baea98db1ebaed3c87ba1169dbda651f4551a524
spokenbird <spokenbird@gmail.com>
2019-07-22 22:45:50 -0700

    Setting up App Build
    Setting up Gulp.

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('app/scss/styles.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+});
+
+gulp.task('watch', function(){
+  gulp.watch('app/scss/styles.scss', gulp.series('styles')
+  // Other watchers
+})
